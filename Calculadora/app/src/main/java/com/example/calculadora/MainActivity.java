cea4afa6e9789804286f58b0da807526a6086159
HeloisaGalvao <heloisa0rayane@gmail.com>
2019-03-18 00:03:22 -0300

    Inicio da calculadora

---
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.style.BulletSpan;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView calculo;
+    private String display = "";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        calculo = findViewById(R.id.txt_calculo);
+        calculo.setText(display);
+    }
+
+    private void updateScreen(){
+        calculo.setText(display);
+    }
+
+    public void onClickNumber(View v){
+        Button b = (Button) v;
+        display += b.getText();
+        updateScreen();
+    }
+
+}
